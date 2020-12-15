d37ad94330358af6986e3ede6724e23f8de1d42f
Zineb BALADOU <zinebbaladou@gmail.com>
2020-12-15 11:27:08 +0100

    update for deploy

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/my-dream-app'));
+app.use(express.static('./dist/my-dream-app'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/my-dream-app/index.html'));
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/my-dream-app/'}),
+);
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
